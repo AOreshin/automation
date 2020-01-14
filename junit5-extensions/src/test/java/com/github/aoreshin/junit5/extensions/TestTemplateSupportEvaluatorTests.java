8c7e583e4abc288980c85e0131495e6e5eec0ef8
besplatnayahurma <hummr65@gmail.com>
2020-01-14 11:40:49 +0300

    Applied spotless

--- junit5-extensions/src/test/java/com/github/aoreshin/junit5/extensions/TestTemplateSupportEvaluatorTests.java
+++ junit5-extensions/src/test/java/com/github/aoreshin/junit5/extensions/TestTemplateSupportEvaluatorTests.java
@@ -1,173 +1,153 @@
 package com.github.aoreshin.junit5.extensions;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.lang.reflect.Method;
-import java.util.Optional;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 class TestTemplateSupportEvaluatorTests {
-    class TestClass {}
-    class AnotherTestClass {}
+  class TestClass {}
 
-    @Test
-    void constructorTestClassTest() {
-        ExtensionContext context = mock(ExtensionContext.class);
-        when(context.getTestClass()).thenReturn(Optional.of(TestClass.class));
+  class AnotherTestClass {}
 
-        Method method = mock(Method.class);
-        String testMethodName = "awesomeTest";
-        when(method.getName()).thenReturn(testMethodName);
-        when(context.getTestMethod()).thenReturn(Optional.of(method));
+  @Test
+  void constructorTestClassTest() {
+    ExtensionContext context = mock(ExtensionContext.class);
+    when(context.getTestClass()).thenReturn(Optional.of(TestClass.class));
 
-        TestTemplateSupportEvaluator evaluator = new TestTemplateSupportEvaluator(context);
+    Method method = mock(Method.class);
+    String testMethodName = "awesomeTest";
+    when(method.getName()).thenReturn(testMethodName);
+    when(context.getTestMethod()).thenReturn(Optional.of(method));
 
-        assertEquals(TestClass.class, evaluator.getTestClass());
-        assertEquals(TestClass.class.getSimpleName(), evaluator.getTestClassSimpleName());
-        assertEquals(testMethodName, evaluator.getTestMethodName());
-    }
+    TestTemplateSupportEvaluator evaluator = new TestTemplateSupportEvaluator(context);
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassTrue(TestTemplateSupportEvaluator evaluator) {
-        boolean result = evaluator
-                .support(TestClass.class)
-                .evaluate();
+    assertEquals(TestClass.class, evaluator.getTestClass());
+    assertEquals(TestClass.class.getSimpleName(), evaluator.getTestClassSimpleName());
+    assertEquals(testMethodName, evaluator.getTestMethodName());
+  }
 
-        assertTrue(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassTrue(TestTemplateSupportEvaluator evaluator) {
+    boolean result = evaluator.support(TestClass.class).evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassFalse(TestTemplateSupportEvaluator evaluator) {
-        boolean result = evaluator
-                .support(AnotherTestClass.class)
-                .evaluate();
+    assertTrue(result);
+  }
 
-        assertFalse(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassFalse(TestTemplateSupportEvaluator evaluator) {
+    boolean result = evaluator.support(AnotherTestClass.class).evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassNameTrue(TestTemplateSupportEvaluator evaluator) {
-        boolean result = evaluator
-                .support("TestClass")
-                .evaluate();
+    assertFalse(result);
+  }
 
-        assertTrue(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassNameTrue(TestTemplateSupportEvaluator evaluator) {
+    boolean result = evaluator.support("TestClass").evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassNameFalse(TestTemplateSupportEvaluator evaluator) {
-        boolean result = evaluator
-                .support("Blahblah")
-                .evaluate();
+    assertTrue(result);
+  }
 
-        assertFalse(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassNameFalse(TestTemplateSupportEvaluator evaluator) {
+    boolean result = evaluator.support("Blahblah").evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassAndMethodNamesTrue(TestTemplateSupportEvaluator evaluator, String testMethodName) {
-        boolean result = evaluator
-                .support(TestClass.class, testMethodName)
-                .evaluate();
+    assertFalse(result);
+  }
 
-        assertTrue(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassAndMethodNamesTrue(
+      TestTemplateSupportEvaluator evaluator, String testMethodName) {
+    boolean result = evaluator.support(TestClass.class, testMethodName).evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassAndMethodNamesWrongMethodName(TestTemplateSupportEvaluator evaluator) {
-        boolean result = evaluator
-                .support(TestClass.class, "someRandomNameWhoCares")
-                .evaluate();
+    assertTrue(result);
+  }
 
-        assertFalse(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassAndMethodNamesWrongMethodName(TestTemplateSupportEvaluator evaluator) {
+    boolean result = evaluator.support(TestClass.class, "someRandomNameWhoCares").evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassAndMethodNamesWrongTestClass(TestTemplateSupportEvaluator evaluator, String testMethodName) {
-        boolean result = evaluator
-                .support(AnotherTestClass.class, testMethodName)
-                .evaluate();
+    assertFalse(result);
+  }
 
-        assertFalse(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassAndMethodNamesWrongTestClass(
+      TestTemplateSupportEvaluator evaluator, String testMethodName) {
+    boolean result = evaluator.support(AnotherTestClass.class, testMethodName).evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassSimpleNameAndMethodNamesTrue(TestTemplateSupportEvaluator evaluator, String testMethodName) {
-        boolean result = evaluator
-                .support(TestClass.class.getSimpleName(), testMethodName)
-                .evaluate();
+    assertFalse(result);
+  }
 
-        assertTrue(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassSimpleNameAndMethodNamesTrue(
+      TestTemplateSupportEvaluator evaluator, String testMethodName) {
+    boolean result = evaluator.support(TestClass.class.getSimpleName(), testMethodName).evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassSimpleNameAndMethodNamesWrongMethodName(TestTemplateSupportEvaluator evaluator) {
-        boolean result = evaluator
-                .support(TestClass.class.getSimpleName(), "someRandomNameWhoCares")
-                .evaluate();
+    assertTrue(result);
+  }
 
-        assertFalse(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassSimpleNameAndMethodNamesWrongMethodName(
+      TestTemplateSupportEvaluator evaluator) {
+    boolean result =
+        evaluator.support(TestClass.class.getSimpleName(), "someRandomNameWhoCares").evaluate();
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestClassSimpleNameAndMethodNamesWrongClassName(TestTemplateSupportEvaluator evaluator, String testMethodName) {
-        boolean result = evaluator
-                .support(AnotherTestClass.class.getSimpleName(), testMethodName)
-                .evaluate();
+    assertFalse(result);
+  }
 
-        assertFalse(result);
-    }
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestClassSimpleNameAndMethodNamesWrongClassName(
+      TestTemplateSupportEvaluator evaluator, String testMethodName) {
+    boolean result =
+        evaluator.support(AnotherTestClass.class.getSimpleName(), testMethodName).evaluate();
 
+    assertFalse(result);
+  }
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestMethodTrue(TestTemplateSupportEvaluator evaluator, String testMethodName) {
-        boolean result = evaluator
-                .supportTestMethod(testMethodName)
-                .evaluate();
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestMethodTrue(TestTemplateSupportEvaluator evaluator, String testMethodName) {
+    boolean result = evaluator.supportTestMethod(testMethodName).evaluate();
 
-        assertTrue(result);
-    }
+    assertTrue(result);
+  }
 
-    @ParameterizedTest
-    @MethodSource("argumentsStream")
-    void supportTestMethodFalse(TestTemplateSupportEvaluator evaluator) {
-        boolean result = evaluator
-                .supportTestMethod("someRandomNameWhoCares")
-                .evaluate();
+  @ParameterizedTest
+  @MethodSource("argumentsStream")
+  void supportTestMethodFalse(TestTemplateSupportEvaluator evaluator) {
+    boolean result = evaluator.supportTestMethod("someRandomNameWhoCares").evaluate();
 
-        assertFalse(result);
-    }
+    assertFalse(result);
+  }
 
-    private static Stream<Arguments> argumentsStream() {
-        ExtensionContext context = mock(ExtensionContext.class);
-        when(context.getTestClass()).thenReturn(Optional.of(TestClass.class));
+  private static Stream<Arguments> argumentsStream() {
+    ExtensionContext context = mock(ExtensionContext.class);
+    when(context.getTestClass()).thenReturn(Optional.of(TestClass.class));
 
-        Method method = mock(Method.class);
-        String testMethodName = "awesomeTest";
-        when(method.getName()).thenReturn(testMethodName);
-        when(context.getTestMethod()).thenReturn(Optional.of(method));
+    Method method = mock(Method.class);
+    String testMethodName = "awesomeTest";
+    when(method.getName()).thenReturn(testMethodName);
+    when(context.getTestMethod()).thenReturn(Optional.of(method));
 
-        TestTemplateSupportEvaluator evaluator = new TestTemplateSupportEvaluator(context);
+    TestTemplateSupportEvaluator evaluator = new TestTemplateSupportEvaluator(context);
 
-        return Stream.of(Arguments.of(
-                evaluator, testMethodName
-        ));
-    }
+    return Stream.of(Arguments.of(evaluator, testMethodName));
+  }
 }
